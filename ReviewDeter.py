ac224f0f5059c8fe26c78f3e8da93e1618d93f95
WoodyYung <21226970@link.hkbu.edu.hk>
2025-03-27 18:44:04 +0800

    function 1 works by nltk

--- ReviewDeter.py
+++ ReviewDeter.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
 
 reviewText = input("Enter review: ") 
 lower_case = reviewText.lower() 
@@ -17,8 +18,13 @@ deter_text = []
 for word in token_text:
     if word not in stopwords.words('english'):
         deter_text.append(word)
-
 #print(deter_text)
+
+# Lemmatization - From plural to single + Base form of a word (example better-> good)
+lemma_words = []
+for word in deter_text:
+    word = WordNetLemmatizer().lemmatize(word)
+    lemma_words.append(word)
 
 # emotion analysis
 emotion_list = []
@@ -29,24 +35,32 @@ with open ('emotion.txt','r') as emofile:
         # store words
         word, emotion = clear_line.split(':')
         #print("Text: "+ word + " " + "Emotion: " + emotion)
-
         # check if word is present
-        if word in deter_text:
+        if word in lemma_words:
             emotion_list.append(emotion)
 
-print(emotion_list)
-w = Counter(emotion_list)
-print(w)
+# print(emotion_list)
+# w = Counter(emotion_list)
+# print(w)
 
 def sentiment_analyze(sentiment_text):
     score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    neg = score['neg']
+    pos = score['pos']
+    if neg > pos:
+        print("Negative Sentiment")
+    elif pos > neg:
+        print("Positive Sentiment")
+    else:
+        print("Neutral Sentiment")
     print(score)
 
 sentiment_analyze(clean_text)
-# plot emotions on graph
 
-fig , axl = plt.subplots()
-axl.bar(w.keys(),w.values())
-fig.autofmt_xdate()
-plt.savefig('pie.png')
-plt.show()
+# plot emotions on graph
+# fig , axl = plt.subplots()
+# axl.bar(w.keys(),w.values())
+# fig.autofmt_xdate()
+# plt.savefig('graph.png')
+# plt.show()
+
